cec4b2e5068a8583abf6ae53c74607d3e726924e
Sukanya Gurav <sukanyagurav6@gmail.com>
2023-01-31 22:15:36 +0530

    completed

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const shareBtn = document.querySelector('.btn')
 const shareOptions = document.querySelector('.share__options')
 
 shareBtn.addEventListener('click',()=>{
+   
     shareOptions.classList.toggle('active')
 })
